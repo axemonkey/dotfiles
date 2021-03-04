e0ad3498810f1e30c9a8bcabbbee87373efc6198
axemonkey <hi+github@clivemurray.com>
2021-03-04 16:19:43 +0000

    [CM] tidy up dotfiles

---
+++ aliases.sh
@@ -0,0 +1,40 @@
+alias be='bundle exec'
+alias l='ls -la'
+alias ..='cd ../; l'
+alias fucking='sudo'
+alias please='sudo'
+
+
+alias gco='git checkout'
+alias gci='git commit'
+alias grb='git rebase'
+alias gst='git status -s'
+alias gdi='git diff'
+alias gitpull='git pull'
+alias gp='git pull'
+alias gadd='git add -A .'
+alias gin='git commit -am'
+alias gitpush='git push'
+alias gitpus='git push'
+alias gpus='git push'
+
+alias ios='open /Applications/Xcode.app/Contents/Developer/Applications/iOS\ Simulator.app'
+
+alias gandipush='git push gandi main'
+alias sshdeploy='ssh 69291@git.sd6.gpaas.net deploy default.git main'
+alias gandishove='gandipush && sshdeploy'
+
+alias basp='subl ~/.bash_profile'
+alias sortit='bundle install; be rake db:migrate'
+alias dontgivemeallthatmate='bundle install; be rake db:migrate'
+alias gitffs='git co main && git pull && git co - && git rebase main'
+alias gmup='git co main && git pull'
+alias gdup='git co development && git pull'
+
+alias drm='docker rm $(docker ps -aq)'
+alias dcu='docker-compose up'
+alias dcd='docker-compose down'
+alias dcs='docker-compose stop'
+alias dcr='docker-compose pull && docker-compose build && docker-compose up'
+
+alias fp="echo -e '                       _______...\n                   .-‘”..........``~.\n               ,.-”..................“-.\n             ,/........................”:\n           ,?............................\ \n          /..............................,}\n         /...........................,:\`^\`.}\n        /..........................,:”..../\n       ?...__......................:\`...../\n       /__.(...“~-,_.............,:\`...../\n      /(_..”~,_....“~,_..........:\`...._/\n     {._$;_...”=,_....“-,_...-~/~},.~”/.}\n      ((...*~_....”=-._...“;,./\`./”...../\n       \\\`~,...“~.,..........\`...}....../\n      (..\`=-,,....\`............(...\_,-”\n      /.\`~,...\`-................\\../^\\ \n       \\\`~.*-,...................|/...\,__\n,,_     }.>-._\..................|........\`=~-,.. \n...\`=~-,_\\_...\`\\,.................\\.............. \n..........\`=~-,,.\\,................\\............. \n................\`:,,..............\`\\.......__.... \n...................\`=-,..........,%\`>--==\`\`...... \n...................._\\......_,-%....\`\\........... \n..................,<\`._|_,-&\`\`........\`\\.........'"
